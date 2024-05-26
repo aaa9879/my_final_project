f9e88df365b92b9c4ed71693e360485cb8cbc5b1
Wang Sheng He <134773871+aaa9879@users.noreply.github.com>
2024-05-26 15:05:36 +0800

    Update views.py

--- line_bot_app/views.py
+++ line_bot_app/views.py
@@ -2,22 +2,23 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.conf import settings
-from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden,JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
-
+from django.views.decorators.http import require_http_methods
 from urllib.parse import parse_qsl
 from static import *
-
+from langchain_openai import ChatOpenAI
 from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
 from linebot.models import *
-from line_bot_app.models import PersonalTable
+from line_bot_app.models import *
 from module import func
-
+import json,os
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
-
+os.environ["OPENAI_API_KEY"] = settings.OPENAI_API_KEY
+llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
 
 @csrf_exempt
 def callback(request):
@@ -33,6 +34,7 @@ def callback(request):
             return HttpResponseBadRequest()
 
         for event in events:
+            print(json.dumps(event, default=lambda o: o.__dict__, indent=4))  # 打印 event 内容
             if isinstance(event, MessageEvent):
                 user_id = event.source.user_id  # 取得LINE ID
                 profile = line_bot_api.get_profile(user_id)
@@ -46,20 +48,96 @@ def callback(request):
                     unit2.save()
                 if isinstance(event.message, TextMessage):
                     mtext = event.message.text
-                    if mtext == '我的帳本':
-                        func.MyAccount(event)
-                    elif mtext[:6] == '<我的帳本>' and len(mtext) > 3:
-                        func.MyAccount(event)
-                    elif mtext == '建立群組':
-                        func.creatgroup(event)
-                    elif mtext[:6] == '<建立群組>':
+                    if mtext[:6] == '<建立群組>':
                         reply_message = func.CreateGroup(mtext, user_id)
                         line_bot_api.reply_message(event.reply_token, TextMessage(text=reply_message))
-                    elif mtext == '加入群組':
-                        func.joingroup(event)
                     elif mtext[:6] == '<加入群組>':
                         reply_message = func.JoinGroup(mtext, user_id)
                         line_bot_api.reply_message(event.reply_token, TextMessage(text=reply_message))
+                    elif mtext[:7] == '<使用者輸入>':
+                        func.classfication(mtext[len('<使用者輸入>'):],user_id,"支出")
         return HttpResponse()
     else:
         return HttpResponseBadRequest()
+
+def search(request):
+    if request.method == 'GET':
+        return render(request, 'Line_liff_search.html')
+
+def liff_add(request):
+    if request.method == 'GET':
+        return render(request, 'Line_liff_add.html')
+#new
+def personal_account_form(request):#new
+    if request.method == 'GET':
+        return render(request, 'Line_liff_personal_form.html')
+@csrf_exempt
+@require_http_methods(["POST", "OPTIONS"])
+def get_user_account(request):
+    if request.method == "OPTIONS":
+        response = HttpResponse()
+        response['Allow'] = 'POST, OPTIONS'
+        return response
+    if request.method == 'POST':
+        try:
+            received_data = json.loads(request.body)
+            line_id = received_data
+            user_instance = PersonalTable.objects.get(personal_id=line_id)
+            user_account = PersonalAccountTable.objects.filter(personal=user_instance)
+
+            account_list = []
+            for account in user_account:
+                category_instance = account.category
+                category_name = category_instance.category_name if category_instance else None
+                account_data = {
+                    "personal_account_id": account.personal_account_id,
+                    "item": account.item,
+                    "account_date": account.account_date.strftime(
+                        '%Y-%m-%d %H:%M:%S') if account.account_date else None,
+                    "location": account.location,
+                    "payment": account.payment,
+                    "flag": account.flag,
+                    "personal_id": account.personal.personal_id,
+                    "category_id": account.category.personal_category_id,
+                    "category_name":category_name
+
+                }
+                account_list.append(account_data)
+
+            response_data = {
+                "message": "Data received successfully",
+                "accounts": account_list
+            }
+            return HttpResponse(json.dumps(response_data), content_type="application/json")
+        except json.JSONDecodeError:
+            return HttpResponse('Invalid JSON', status=400)
+        except PersonalTable.DoesNotExist:
+            return HttpResponse('User not found', status=404)
+        except Exception as e:
+            return HttpResponse(str(e), status=500)
+    else:
+        return HttpResponse('Only POST requests are allowed', status=405)
+
+#new
+@csrf_exempt
+@require_http_methods(["POST", "OPTIONS"])
+def get_user_account_info(request):
+    if request.method == "OPTIONS":
+        response = HttpResponse()
+        response['Allow'] = 'POST, OPTIONS'
+        return response
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            user_id = data.get('userId')
+            input_text = data.get('input')
+            transaction_type = data.get('type')
+            response_data = {'message': '成功接收數據', 'userId': user_id, 'input': input_text}
+            temp = func.classification(input_text,user_id,transaction_type)
+            return JsonResponse({**response_data,'temp':temp})
+        except json.JSONDecodeError:
+            return JsonResponse({'error': '無效的JSON數據'}, status=400)
+        else:
+            return JsonResponse({'error': '支持POST請求'}, status=405)
+
+
